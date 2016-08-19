971a12642cba090d188a2f759bca2771654af98e
Caleb Curry <clbcrry@hotmail.com>
2016-08-19 19:53:44 -0400

    Refactor Javascript.  Create a new function to write content to the page rather than polluting the user's experience with console logs and alerts.

--- CTVM2App/Scripts/SignalR.js
+++ CTVM2App/Scripts/SignalR.js
@@ -1,12 +1,17 @@
 ﻿(function () {
+    var myHub = $.connection.myHub;
     $.connection.hub.start()
         .done(function () {
-            console.log("IT WORKED!");
-            $.connection.myHub.server.announce("Connected!");
+            writeToPage("It worked!");
+            myHub.server.announce("Connected!");
         })
-        .fail(function () { alert("ERROR!!!!"); });
+        .fail(function () { writeToPage("Error connecting to SignalR"); });
 
-    $.connection.myHub.client.announce = function (message) {
+    myHub.client.announce = function (message) {
+        writeToPage(message);
+    }
+
+    var writeToPage = function (message) {
         $("#welcome-messages").append(message + "<br />");
     }
 })()
