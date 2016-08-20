c977245b5ac849cce0968b7106a915a97be92d64
Caleb Curry <clbcrry@hotmail.com>
2016-08-20 13:51:58 -0400

    Enable client side logging.

--- CTVM2App/Scripts/SignalR.js
+++ CTVM2App/Scripts/SignalR.js
@@ -1,21 +1,15 @@
 ﻿(function () {
-    $("#click-me").on("click", function () {
-        myHub.server.getServerDateTime()
-           .done(function (data) {
-               writeToPage(data);
-           })
-           .fail(function (e) {
-               writeToPage(e);
-           });
-    })
-
     var chat = $.connection.chat;
+
     $.connection.hub.start()
         .done(function () {
-            writeToPage("It worked!");
+            $.connection.hub.logging = true;
+            $.connection.hub.log("Connection established");
             chat.server.announceToEverybody("Connected!");
         })
-        .fail(function () { writeToPage("Error connecting to SignalR"); });
+        .fail(function () {
+            writeToPage("Error connecting to SignalR");
+        });
 
     chat.client.announce = function (message) {
         writeToPage(message);
@@ -24,4 +18,14 @@
     var writeToPage = function (message) {
         $("#welcome-messages").append(message + "<br />");
     }
+
+    $("#click-me").on("click", function () {
+        chat.server.getServerDateTime()
+           .done(function (data) {
+               writeToPage(data);
+           })
+           .fail(function (e) {
+               writeToPage(e);
+           });
+    })
 })()
